6af51a4ec98c3b5bdb366489eaa27eb449dca683
Doudoumouna <eboalucien@yahoo.it>
2023-06-01 21:13:46 +0200

    First playable version

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss'],
+})
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  currentCard = '';
+  game!: Game;
+
+  constructor(public dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame(): void {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard(): void {
+    if (!this.pickCardAnimation) {
+      const poppedCard = this.game.stack.pop();
+      if (typeof poppedCard === 'string') {
+        this.currentCard = poppedCard;
+        this.pickCardAnimation = true;
+
+        this.game.currentPlayer++;
+        this.game.currentPlayer =
+          this.game.currentPlayer % this.game.players.length;
+        setTimeout(() => {
+          this.game.playedCards.push(this.currentCard);
+          this.pickCardAnimation = false;
+        }, 1000);
+      }
+    }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      if (name && name.length > 0) {
+        this.game.players.push(name);
+      }
+    });
+  }
+}
---
+++ src/app/models/game.ts
@@ -0,0 +1,35 @@
+export class Game {
+  public players: string[] = [];
+  public player_images: string[] = [];
+  public stack: string[] = [];
+  public playedCards: string[] = [];
+  public currentPlayer: number = 0;
+  public pickCardAnimation = false;
+  public currentCard: string = '';
+
+  constructor() {
+    for (let i = 1; i < 14; i++) {
+      this.stack.push('ace_' + i);
+      this.stack.push('hearts_' + i);
+      this.stack.push('clubs_' + i);
+      this.stack.push('diamonds_' + i);
+    }
+
+    this.shuffle(this.stack);
+  }
+
+  private shuffle(array: any[]): void {
+    let currentIndex = array.length,
+      randomIndex;
+
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+  }
+}
